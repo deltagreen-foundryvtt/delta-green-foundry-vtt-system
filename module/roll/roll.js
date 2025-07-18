264d3ccbff3d180f295373257f9db4136cd027a3
reyzor1991 <mishabudjakov@gmail.com>
2025-07-18 17:53:42 +0300

    Update module/roll/roll.js
    Co-authored-by: Jalen <72114365+jalensailin@users.noreply.github.com>

--- module/roll/roll.js
+++ module/roll/roll.js
@@ -409,8 +409,8 @@ export class DGPercentileRoll extends DGRoll {
   }
 
   get exhausted() {
-    let isExhausted = false;
-    let exhaustedCheckPenalty = -20;
+    const isExhausted = false;
+    const exhaustedCheckPenalty = -20;
 
     try {
       // I suspect (but am not entirely certain) that being tired doesn't make you less lucky)
