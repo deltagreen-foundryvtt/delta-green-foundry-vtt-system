61361d63bd2b10de036fab763c978598d686afda
Mykhailo Budiakov <mishabudjakov@gmail.com>
2025-08-31 22:16:22 +0300

    fix lint

--- module/deltagreen.js
+++ module/deltagreen.js
@@ -85,7 +85,7 @@ Hooks.once("setup", async () => {
   // Register item sheet.
   Items.registerSheet(DG.ID, DeltaGreenItemSheet, {
     makeDefault: true,
-    label: game.i18n.format("DG.TypedSheet", { type: 'Item' }),
+    label: game.i18n.format("DG.TypedSheet", { type: "Item" }),
     themes: null,
   });
 });
