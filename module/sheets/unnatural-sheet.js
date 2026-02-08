dcf38942de93c964b0dcfed4edf0994a1b004fcb
Mykhailo Budiakov <mishabudjakov@gmail.com>
2026-02-08 21:32:40 +0200

    add stats edit form

--- module/sheets/unnatural-sheet.js
+++ module/sheets/unnatural-sheet.js
@@ -14,9 +14,9 @@ export default class DGUnnaturalSheet extends DGNPCSheet {
     // we need to update max HP if STR or CON have changed (only for unnatural, but doesn't hurt others)
     submittedData.system.health.maxNeedsUpdate =
       submittedData.system?.statistics.str.value !==
-      this.actor.system.statistics.str.value ||
+        this.actor.system.statistics.str.value ||
       submittedData.system?.statistics.con.value !==
-      this.actor.system.statistics.con.value;
+        this.actor.system.statistics.con.value;
 
     return super._processSubmitData(event, form, submittedData, options);
   }
