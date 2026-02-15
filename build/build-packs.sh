66106a218b6681cba44d211e3a50a522c0254dd3
jalensailin <jalen.michalslevy@gmail.com>
2026-02-15 13:33:56 -0500

    Fix variable quoting issue in build-packs.sh

--- build/build-packs.sh
+++ build/build-packs.sh
@@ -5,6 +5,6 @@ IFS=$'\n\t'
 npx --no-install fvtt package workon "deltagreen" --type "System"
 
 for folder in packs/source/*; do
-    name=$(basename $folder)
+    name=$(basename "$folder")
     npx --no-install fvtt package pack "$name" --in "$folder" --out "packs"
 done
