42c682c3bda145d2ff61232554d9ad58d13f6d04
jalensailin <jalen.michalslevy@gmail.com>
2026-06-20 00:22:41 -0400

    Fix translating "New Item" string

--- module/sheets/base-actor-sheet.js
+++ module/sheets/base-actor-sheet.js
@@ -752,12 +752,9 @@ export default class DGActorSheet extends DGSheetMixin(ActorSheetV2) {
    */
   _onItemCreate(type) {
     // Initialize a default name.
-    const name = game.i18n.format(
-      game.i18n.translations.DOCUMENT?.New || "DG.FallbackText.newItem",
-      {
-        type: game.i18n.localize(`TYPES.Item.${type}`),
-      },
-    );
+    const name = game.i18n.format("DG.FallbackText.newItem", {
+      type: game.i18n.localize(`TYPES.Item.${type}`),
+    });
 
     // Prepare the item object.
     const itemData = {
