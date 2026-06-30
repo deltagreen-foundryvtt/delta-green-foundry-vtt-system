0cb1c7a1fefe01b2f81d9c9a8deea617f2034d85
Chris Saunders <chris@christophersaunders.ca>
2026-06-29 20:01:23 -0400

    WIP: tests

--- module/macros/stat-block-parser.js
+++ module/macros/stat-block-parser.js
@@ -11,11 +11,53 @@ export const States = {
   Attack: "attack",
 };
 
-class StatblockParser {
+const Attributes = {
+  str: "STR",
+  dex: "DEX",
+  con: "CON",
+  int: "INT",
+  pow: "POW",
+  cha: "CHA",
+  hp: "HP",
+  wp: "WP",
+  san: "SAN",
+};
+
+export function tokenize(stream) {
+  return stream.split(/\s+/);
+}
+
+export class StatblockParser {
   constructor(stream) {
     this.state = States.BeginStatblock;
-    this.tokens = stream.split(/\s+/);
+    this.tokens = tokenize(stream);
   }
 }
 
-export default StatblockParser;
+export function ExtractAttributes(tokens, previousState) {
+  if (tokens === []) {
+    return [previousState, tokens];
+  }
+
+  const [rawAttr, rawValue, ...rest] = tokens;
+  let accumulator = previousState;
+  if (previousState === undefined) {
+    accumulator = { incomplete: true };
+  }
+  const attr = rawAttr.toLowerCase();
+  const value = parseInt(rawValue);
+  const currentKeys = Object.keys(accumulator);
+  if (
+    currentKeys !== ["incomplete"] &&
+    currentKeys - Object.keys(Attributes) === ["incomplete"]
+  ) {
+    delete accumulator.incomplete;
+  }
+
+  if (Attributes[attr] == null || Number.isNaN(value)) {
+    return [accumulator, tokens];
+  }
+
+  accumulator[attr] = value;
+  return ExtractAttributes(rest, accumulator);
+}
--- test/macros/stat-block-parser.test.js
+++ test/macros/stat-block-parser.test.js
@@ -1,7 +1,11 @@
 import { describe, expect, test } from "@jest/globals";
 import fs from "node:fs";
 
-import StatblockParser from "../../module/macros/stat-block-parser.js";
+import {
+  StatblockParser,
+  ExtractAttributes,
+  tokenize,
+} from "../../module/macros/stat-block-parser.js";
 
 describe("StatblockParser", () => {
   const readStatblock = (name) =>
@@ -12,6 +16,47 @@ describe("StatblockParser", () => {
       const parser = new StatblockParser(readStatblock("basic-stats"));
       expect(parser.tokens.length).toBeGreaterThan(0);
       console.log(parser.tokens);
+    });
+  });
+});
+
+describe("ExtractAttributes", () => {
+  const validAttributes =
+    "STR 10 con 10 Dex 13 Int 10 pOW 14 ChA 10\n\nHp 10 wp 14\n\t\nsaN 10\nSKILLS:";
+  const invalidAttributes = "Bear 10 Criminal 2 STr 10 con 11 Int 10";
+  const incompleteAttributes = "Str 10 con 10 Dex 10 10 Int";
+
+  test("extracting valid attributes", () => {
+    const { result, rest } = ExtractAttributes(tokenize(validAttributes));
+    expect(rest).toBe(["SKILLS:"]);
+    expect(result).toBe({
+      str: 10,
+      dex: 13,
+      con: 10,
+      int: 10,
+      pow: 14,
+      cha: 10,
+      hp: 10,
+      wp: 14,
+      san: 10,
+    });
+  });
+
+  test("extracting invalid attributes", () => {
+    const tokens = tokenize(invalidAttributes);
+    const { result, rest } = ExtractAttributes(tokens);
+    expect(rest).toBe(tokens);
+    expect(result).toBe({ incomplete: true });
+  });
+
+  test("extracting incomplete attributes", () => {
+    const { result, rest } = ExtractAttributes(tokenize(incompleteAttributes));
+    expect(rest).toBe(["10", "Int"]);
+    expect(result).toBe({
+      str: 10,
+      con: 10,
+      dex: 10,
+      incomplete: true,
     });
   });
 });
